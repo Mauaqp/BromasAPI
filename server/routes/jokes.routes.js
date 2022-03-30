c115dc558815a834fe39dd066a44d485e092f3fe
Mauaqp <peraltilla93@hotmail.com>
2022-03-30 10:14:01 -0500

    Corrección

--- server/routes/jokes.routes.js
+++ server/routes/jokes.routes.js
@@ -4,7 +4,7 @@ module.exports = app => {
     app.get("/api/jokes/", JokeController.findAllJokes);
     app.get("/api/jokes/:id", JokeController.findOneSingleJoke);
     app.get("/api/jokes/random", JokeController.findRandomJoke);
-    app.get("/api/jokes/new", JokeController.createNewJoke);
-    app.get("/api/jokes/update/:id", JokeController.updateExistingJoke);
-    app.get("/api/jokes/delete/:id", JokeController.deleteAnExistingJoke);
+    app.post("/api/jokes/new", JokeController.createNewJoke);
+    app.update("/api/jokes/update/:id", JokeController.updateExistingJoke);
+    app.delete("/api/jokes/delete/:id", JokeController.deleteAnExistingJoke);
 };
